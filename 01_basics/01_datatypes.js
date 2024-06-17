02ae624d3c11eb348d790779a83709a8652b6b2a
Malya Srivastava <125035993+sriimalya@users.noreply.github.com>
2024-06-17 09:32:21 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Hello")
+
+
+let name = "malya"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// primitive datatypes
+// number => 2 to power 53
+// bigint => for large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value (object type)
+// undefined => kindda place holder for a value which is not defined yet (type = undefined)
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
