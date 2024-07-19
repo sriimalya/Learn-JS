bb49959335c428a375c6decfd76e10d309eae0d8
Malya Srivastava <125035993+sriimalya@users.noreply.github.com>
2024-07-19 19:11:49 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+// to avaoid polution from global scope
+// ()() : (func def)(execute)
+
+
+// named iife : chai named
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+//; semicolon is necessary toexecute multiple iife
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('malya')
