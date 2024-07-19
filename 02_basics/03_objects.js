b31bf86edb296af1456303d60121fe994b35c099
Malya Srivastava <125035993+sriimalya@users.noreply.github.com>
2024-07-19 16:53:30 +0000

    json

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,7 +9,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Malya",
     "full name": "Malya Srivastava",
-    // Symbols have to declared in [], else they are comverted to string type
+    // Symbols have to be declared in [], else they are converted to string type
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
@@ -24,10 +24,10 @@ const JsUser = {
 // console.log(JsUser[mySym]) // to access Symbol datatypes
 
 
-console.log(typeof JsUser.age)
-//JsUser.email = "malya@chatgpt.com"
+// console.log(typeof JsUser.age) // number
+// JsUser.email = "malya@chatgpt.com"
 // Object.freeze(JsUser) // to avoid any changes
-//JsUser.email = "malya@microsoft.com"
+// JsUser.email = "malya@microsoft.com" // won't reflect if freezed
 // console.log(JsUser);
 
 JsUser.greeting = function(){
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,14 +1,20 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() // singleton object
+// console.log(tinderUser) // {} // empty object
+
+const tinderUser = {} // non-singleton object
+// console.log(tinderUser) // {} // empty object
+
 
 tinderUser.id = "123abc"
 tinderUser.name = "Malya"
 tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
+// console.log(typeof tinderUser);
 
 const regularUser = {
     email: "some@gmail.com",
+    // nesting of object
     fullname: {
         userfullname: {
             firstname: "malya",
@@ -17,6 +23,7 @@ const regularUser = {
     }
 }
 
+// ? is used for optional chaining
 // console.log(regularUser.fullname?.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
@@ -24,12 +31,13 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 } // results in nesting of objects
+
 // const obj3 = Object.assign({}, obj1, obj2, obj4) // target={}, rest objs are source
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-
+// array of objects 
 const users = [
     {
         id: 1,
@@ -45,16 +53,16 @@ const users = [
     },
 ]
 
-users[1].email
+// users[1].email
 
 // console.log(tinderUser);
 // console.log(Object.keys(tinderUser)); // array of id, name, isLoggedIn
-// console.log(Object.values(tinderUser));
+// console.log(Object.values(tinderUser)); // array of values
 // console.log(Object.entries(tinderUser)); // array in arrray [[key,value],[key,value]...]
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
 
-// deconstruct array
+// deconstruct array and objects
 const course = {
     coursename: "js",
     price: "999",
@@ -66,16 +74,19 @@ const course = {
 const {courseInstructor: instructor} = course
 
 // console.log(courseInstructor);
-console.log(instructor);
+// console.log(instructor);
 
+// json : js object notation
 // {
 //     "name": "malya",
 //     "coursename": "js",
 //     "price": "free"
 // }
 
-[
-    {},
-    {},
-    {}
-]
+
+// array of objects
+// [
+//     {},
+//     {},
+//     {}
+// ]
