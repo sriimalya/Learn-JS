49a794ff35aa296686c8a298d948c8c8e1fc6654
Malya Srivastava <125035993+sriimalya@users.noreply.github.com>
2024-07-20 17:23:39 +0000

    iterations

--- 05_iterations/03_forof.js
+++ 05_iterations/03_forof.js
@@ -5,17 +5,18 @@
 
 const arr = [1, 2, 3, 4, 5]
 
+// array
 for (const num of arr) {
     //console.log(num);
 }
 
+// strings
 const greetings = "Hello world!"
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
 
 // Maps
-
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
